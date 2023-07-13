d61a4d4894c17ecb9c821a2007582233b9d96f16
paramountimpexllc <138040971+paramountimpexllc@users.noreply.github.com>
2023-07-13 03:25:17 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ const about = () => {
             <div className="flex flex-col lg:flex-row justify-between gap-8">
                 <div className="w-full lg:w-5/12 flex flex-col justify-center">
                     <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 ">Welcome to Paramount Impex! We are thrilled to have you here. At Paramount Impex, we are dedicated to providing our customers with an exceptional shopping experience and delivering high-quality products that exceed their expectations. With a commitment to trust and loyalty, we aim to be your go-to destination for all your Store needs.</p>
                 </div>
                 <div className="w-full lg:w-8/12 ">
                     <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
@@ -17,7 +17,7 @@ const about = () => {
             <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
                 <div className="w-full lg:w-5/12 flex flex-col justify-center">
                     <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 ">Our journey began with a simple idea and a passion for service category. As avid product enthusiasts ourselves, we noticed a gap in the market for specific need. We saw an opportunity to create a platform that not only offers a wide range of top-notch products but also prioritizes customer satisfaction and builds lasting relationships.</p>
                 </div>
                 {/* <div className="w-full lg:w-8/12 lg:pt-8">
                     <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
