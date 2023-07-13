f98deea4d886676a14f63187cdbc846fcb0e29ba
unknown <paramountimpexllc.com>
2023-07-13 02:44:18 -0700

    first commit

---
+++ components/Submenu.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Submenu() {
+
+  return (
+    <div className="pb-16" style={{ fontFamily: '"Lato", sans-serif' }}>
+      {/* Code block starts */}
+      <dh-component>
+        <section className="max-w-8xl mx-auto container bg-white pt-16">
+          <div>
+            <div role="contentinfo" className="flex items-center flex-col px-4">
+              <hh1 tabIndex={0} className="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-gray-800 lg:w-5/12 md:w-9/12 pt-4">Create Comfortability and Accessibility &amp;  in a Jiffy</hh1>
+            </div>
+            <div tabIndex={0} aria-label="group of cards" className="focus:outline-none mt-20 flex flex-wrap justify-center gap-10 px-4">
+              <div tabIndex={0} aria-label="card 1" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG1.svg" alt="drawer" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-bold leading-tight text-gray-800">Ready to Ship Products</h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">It provides a very simple start, no need to worry.</p>
+                </div>
+              </div>
+              <div tabIndex={0} aria-label="card 2" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG2.svg" alt="check" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Secure Payment Methods</h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">Modify the Accessibility and Security.</p>
+                </div>
+              </div>
+              <div tabIndex={0} aria-label="card 3" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG3.svg" alt="html tag" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Easier Than Ever</h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">Nothing too complex, Simpler and Beautiful</p>
+                </div>
+              </div>
+              <div tabIndex={0} aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg" alt="monitor" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">The Last Site you’ll ever need</h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">We have everything for the best prices.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </dh-component>
+      {/* Code block ends */}
+    </div>
+
+  )
+}
+
+export default Submenu
