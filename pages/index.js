e062643c9b69e814e09f83b998c887f2ebf7283b
mutiurrehman11 <129205085+mutiurrehman11@users.noreply.github.com>
2023-07-15 07:25:32 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ products }) {
                 {/* heading and paragaph start */}
                 <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Paramount Impex LLC
+                        Paramount Impex
                     </div>
                     <div className="text-md md:text-xl">
                         Shop With Us and Discover the Beauty of Accessibility and Freedom.
