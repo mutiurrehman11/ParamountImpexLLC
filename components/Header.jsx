7e513ab2676fc866e2ed74dfdcc985c9b649ce8a
mutiurrehman11 <129205085+mutiurrehman11@users.noreply.github.com>
2023-07-15 01:42:40 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
     };
 
     return (
-        <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
+        <header className={`w-full h-[50px] md:h-[80px] bg-wzinc-200 flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
              
 
             <Wrapper className="h-[60px] flex justify-between items-center">
