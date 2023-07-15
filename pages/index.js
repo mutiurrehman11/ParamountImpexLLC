9e25dd5c5a763b3b97a3e7f2c2cab511a3039d6c
mutiurrehman11 <129205085+mutiurrehman11@users.noreply.github.com>
2023-07-15 00:14:10 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import HeroBanner from "@/components/HeroBanner";
 import Wrapper from "@/components/Wrapper";
 import submenu from "@/components/Submenu";
-// import ProductCard from "@/components/ProductCard";
+import ProductCard from "@/components/ProductCard";
 import { fetchDataFromApi } from "@/utils/api";
 import Submenu from "@/components/Submenu";
 import CTA from "@/components/CTA";
@@ -24,13 +24,13 @@ export default function Home({ products }) {
                 {/* heading and paragaph end */}
 
                 {/* products grid start */}
-                {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">    
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">    
                     {
                     products?.data?.map((product) => (
                         <ProductCard key={product?.id} data={product} />
                     ))}
 
-                    {/* <ProductCard />
+                {/*     {/* <ProductCard />
                     <ProductCard />
                     <ProductCard />
                     <ProductCard />
@@ -38,8 +38,8 @@ export default function Home({ products }) {
                     <ProductCard />
                     <ProductCard />
                     <ProductCard />
-                    <ProductCard /> 
-                </div>*/}
+                    <ProductCard /> */}
+                </div>
                 {/* products grid end */}
                 <Submenu></Submenu>
                 <CTA/>
