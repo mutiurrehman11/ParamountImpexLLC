b9c5022b38610c55e0ad882c16cbfe1ff8f3312b
mutiurrehman11 <129205085+mutiurrehman11@users.noreply.github.com>
2023-07-15 00:15:55 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
             <Wrapper className="h-[60px] flex justify-between items-center">
                 <Link href="/">
                     {/* <span className="font-bold text-base font-sans hover:italic">Paramount Impex LLC</span> */}
-                    <img src="/logo.png" className="w-[65px] md:w-[170px]" />
+                    <img src="/logo.png" className="w-[105px] md:w-[170px]" />
                 </Link>
 
                 <Menu 
